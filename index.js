cc3a73550734c12c0e22c8829681705c2011c181
Gamebot-site <76865811+Gamebot-site@users.noreply.github.com>
2021-07-09 18:37:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import css from './main.css';
+import favicon from './images/favicon.ico';
+import gameBotBackgroundPrimary from './images/gamebot-background-primary.png';
+import gameBotBackgroundSecondary from './images/gamebot-background-secondary.png';
+import gBot5050 from './images/gbot-5050.png';
+import mashupIcon from './images/mashup-icon.png';
+import sheriff from './images/sheriff.png';
+import thunder from './images/thunder.gif';
+import turkey from './images/turkey.png';
+import waveFooter from './images/wave-footer.png';
+import waveHeader from './images/wave-header.png';
+import waveHeroBackground from './images/wave-hero-background.png'; // Fix later webpack is throwing an error
+import waveLeft from './images/wave-left.png';
